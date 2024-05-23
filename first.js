a99628a3cda9419844419febb80013f6eddbe874
Seokwoodang <swoo1429@naver.com>
2024-05-23 23:48:29 +0900

    first.js 테스트 케이스 추가

--- first.js
+++ first.js
@@ -1,4 +1,3 @@
-
 // 문제 1)
 // snake_case로 구분되어있는 문자열 my_string이 매개변수로 주어질 때, 
 // my_string에 나온 단어를 앞에서부터 순서대로 담은 문자열 배열을 return 하는 solution 함수를 작성해 주세요.
@@ -14,7 +13,6 @@
 
 const first = (my_string)=>{
     let answer;
-    answer = my_string.split('_')
     return answer
 }
 
@@ -22,7 +20,8 @@ const runTests = () => {
     const testCases = [
         { my_string: "i_love_you", expected: ["i", "love", "you"] },
         { my_string: "programmers", expected: ["programmers"] },
-        { my_string: "hello_world_this_is_snake_case", expected: ["hello", "world", "this", "is", "snake", "case"] }
+        { my_string: "hello_world_this_is_snake_case", expected: ["hello", "world", "this", "is", "snake", "case"] },
+        { my_string:"a_b_c_d_e_f_g_h_i_j_k_l_m_n_o_p_q_r_s_t_u_v_w_x_y_z", expected:["a", "b", "c", "d", "e", "f", "g", "h", "i", "j", "k", "l", "m", "n", "o", "p", "q", "r", "s", "t", "u", "v", "w", "x", "y", "z"]}
     ];
 
     testCases.forEach(({ my_string, expected }, index) => {
